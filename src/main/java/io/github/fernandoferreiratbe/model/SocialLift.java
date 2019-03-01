fe76f6672ebc0fd07e2bc7c91834001fa106966f
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-02-28 22:03:33 -0300

    Implement basic lift's go method

--- src/main/java/io/github/fernandoferreiratbe/model/SocialLift.java
+++ src/main/java/io/github/fernandoferreiratbe/model/SocialLift.java
@@ -1,8 +1,11 @@
 package io.github.fernandoferreiratbe.model;
 
+import io.github.fernandoferreiratbe.view.App;
+
 public class SocialLift extends Lift {
 
     private static final int MAX_NUMBER_PEOPLE = 8;
+    private int nextBuildingFloorToVisit = 1;
 
     @Override
     public void setFloorToGo(Floor floor) throws IllegalLiftStateException {
@@ -17,11 +20,36 @@ public class SocialLift extends Lift {
 
     @Override
     public void go() {
-        System.out.println("Social Lift is going...");
+        App.talkToUser("Social Lift is going...");
+        this.getState().setLiftInMovement(true);
+
+        for (Floor floorToGo : super.getFloorsToGo()) {
+            for (; this.nextBuildingFloorToVisit <= super.getBuildingFloors().size(); this.nextBuildingFloorToVisit++) {
+
+                App.talkToUser("FLOOR [ " + this.nextBuildingFloorToVisit + " ]");
+                Floor buildingFloor = super.getBuildingFloors().get(this.nextBuildingFloorToVisit);
+
+                if (floorToGo.getFloor() == buildingFloor.getFloor()) {
+                    App.talkToUser("Stopping Social Lift at floor " + buildingFloor.getFloor());
+                    this.getState().setLiftInMovement(false);
+
+                    App.talkToUser("Open the door");
+                    this.getState().setDoorState(DoorState.OPEN);
+
+                    App.talkToUser("Get out " + floorToGo.getPeopleOnThisFloor() + " from lift.");
+                    this.setNextBuildingFloorToVisit(this.nextBuildingFloorToVisit + 1);
+                    break;
+                }
+            }
+        }
     }
 
     @Override
     public LiftState getState() {
         return super.getState();
+    }
+
+    private void setNextBuildingFloorToVisit(int nextBuildingFloorToVisit) {
+        this.nextBuildingFloorToVisit = nextBuildingFloorToVisit;
     }
 }
